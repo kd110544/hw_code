3079bad8fa40248fb3731dc37bfd39b6ec98672a
JEFFCHAN-ACER-SWIFTSF514 <cwchan@arizona.edu>
2022-10-03 16:50:29 -0700

    finish defining function

--- k-space_analysis_tool_OPTI574_HW3.py
+++ k-space_analysis_tool_OPTI574_HW3.py
@@ -38,131 +38,117 @@ def set_data_points(fov_x,fov_y,points):
 
     return x,y 
 
-def plot_cartesian_space(hfov,vfov,half_fov=15,legend='input',style='s-b'):
+def cartesian_to_k_space(hfov,vfov):
+    kx = []
+    ky = []
+    for i in range(0,len(hfov)):
+        # Define θ (theta)
+        theta_x = np.deg2rad(hfov[i])
+        theta_y = np.deg2rad(vfov[i])
+        tan_theta_x = np.tan(theta_x)
+        tan_theta_y = np.tan(theta_y)
+        theta = np.rad2deg(np.arctan(np.sqrt((tan_theta_x**2)+(tan_theta_y**2))))
+        # Define φ (phi)
+        if tan_theta_x == 0:
+            if tan_theta_y > 0:
+                phi = 90
+            else: 
+                phi = -90
+        else:
+            phi = np.rad2deg(np.arctan2(tan_theta_y,tan_theta_x))
+        # Convert to kx & ky
+        kx.append(np.cos(np.deg2rad(phi)) * np.sin(np.deg2rad(theta)))
+        ky.append(np.sin(np.deg2rad(phi)) * np.sin(np.deg2rad(theta)))
+    
+    return kx,ky
+
+def k_space_to_cartesian(kx,ky):
+    kz = []
+    hfov = []
+    vfov = []
+    for i in range(0,len(kx_after_OG)):
+        # Define kz 
+        kz.append(np.sqrt(1-kx_after_OG[i]**2-ky_after_OG[i]**2))
+        hfov.append(np.rad2deg(np.arctan2(kx[i],kz[i])))
+        vfov.append(np.rad2deg(np.arctan2(ky[i],kz[i])))
+    
+    return hfov,vfov
+
+def model_the_grating(kx, ky, m, lambda_grating, pitch, rotation_angle):
+    m = m # diffacted order
+    lambda_grating = lambda_grating # in nm
+    pitch = pitch # in nm (input grating)
+    rotation_angle = np.deg2rad(rotation_angle)
+    kx_unit_vector = 1
+    ky_unit_vector = 1
+    k1_x = (m*lambda_grating/pitch)*(np.cos(rotation_angle)*kx_unit_vector)
+    k1_y = (m*lambda_grating/pitch)*(np.sin(rotation_angle)*ky_unit_vector)
+    
+    kx_after_grating = []
+    ky_after_grating = []
+    for i in range(0,len(kx)):
+        kx_after_grating.append(kx[i]+k1_x)
+        ky_after_grating.append(ky[i]+k1_y)
+    
+    return kx_after_grating, ky_after_grating
+  
+def plt_cartesian_space(hfov,vfov,half_fov=15,style='s-b'):
     # default axis limit = 30 deg, default legend = 'input'
-    # plt.figure(1,dpi=300)
     plt.plot(hfov,vfov,style,markersize='4')
     plt.title('x/y Cartesian space',fontsize='12')
     plt.xlabel('Cartesian x angle (deg)',fontsize='12')
     plt.ylabel('Cartesian y angle (deg)',fontsize='12')
     plt.xlim([-half_fov,half_fov])
     plt.ylim([-half_fov,half_fov])
-    plt.legend([str(legend)])
     plt.gca().set_aspect('equal','box')
     
+def plt_circle(radius=1):
+    radius = radius # radius = 1 in air
+    angle = np.linspace(0, 2*np.pi, 100) 
+    a = radius * np.cos(angle) 
+    b = radius * np.sin(angle) 
+    plt.plot(a,b,linewidth='1')
+    
 
+# Input system spec here
 fov_x = 30
 fov_y = 18
 points = 5
+substrate_index = 2
+max_angle_allowed_inside_waveguide = np.deg2rad(80)
 
+# get x/y Cartesian data points
 hfov, vfov = set_data_points(fov_x, fov_y, points)
-# plot_cartesian_space(hfov,vfov,legend = 'input')
 
-#%%
+# make a figure
 plt.figure(dpi=600)
-kx = []
-ky = []
 
-# cartesian to k-space
-for i in range(0,len(hfov)):
-    # Define θ (theta)
-    theta_x = np.deg2rad(hfov[i])
-    theta_y = np.deg2rad(vfov[i])
-    tan_theta_x = np.tan(theta_x)
-    tan_theta_y = np.tan(theta_y)
-    theta = np.rad2deg(np.arctan(np.sqrt((tan_theta_x**2)+(tan_theta_y**2))))
-    # Define φ (phi)
-    if tan_theta_x == 0:
-        if tan_theta_y > 0:
-            phi = 90
-        else: 
-            phi = -90
-    else:
-        phi = np.rad2deg(np.arctan2(tan_theta_y,tan_theta_x))
-    # Calculate kx & ky
-    kx.append(np.cos(np.deg2rad(phi)) * np.sin(np.deg2rad(theta)))
-    ky.append(np.sin(np.deg2rad(phi)) * np.sin(np.deg2rad(theta)))
-    
-# plot grating vector
+# Convert x/y Cartesian data points into k-space
+kx,ky = cartesian_to_k_space(hfov, vfov)
 plt.plot(kx,ky,'s-b',markersize='2')
 
-
 ## IG (Iput grating vector)
-m = 1 # diffacted order
-lambda_grating = 640 # in nm
-pitch = 380 # in nm (input grating)
-rotation_angle = np.deg2rad(-90)
-kx_unit_vector = 1
-ky_unit_vector = 1
-k1_x = (m*lambda_grating/pitch)*(np.cos(rotation_angle)*kx_unit_vector)
-k1_y = (m*lambda_grating/pitch)*(np.sin(rotation_angle)*ky_unit_vector)
-
-kx_after_IG = []
-ky_after_IG = []
-for i in range(0,len(kx)):
-    kx_after_IG.append(kx[i]+k1_x)
-    ky_after_IG.append(ky[i]+k1_y)
-    
-# plot IG grating vector
-plt.plot(kx_after_IG,ky_after_IG,'s-y',markersize='2')
-
-
+kx_after_IG, ky_after_IG = model_the_grating(kx, ky, m=1, lambda_grating=640, pitch=380, rotation_angle=-90)
 ## EG (Expansion grating vector)
-# m = 1 # diffacted order
-# lambda_grating = 640 # in nm
-pitch = 268.7 # in nm (expansion grating)
-rotation_angle = np.deg2rad(45)
-kx_unit_vector = 1
-ky_unit_vector = 1
-k2_x = (m*lambda_grating/pitch)*(np.cos(rotation_angle)*kx_unit_vector)
-k2_y = (m*lambda_grating/pitch)*(np.sin(rotation_angle)*ky_unit_vector)
-
-kx_after_EG = []
-ky_after_EG = []
-for i in range(0,len(kx)):
-    kx_after_EG.append(kx_after_IG[i]+k2_x)
-    ky_after_EG.append(ky_after_IG[i]+k2_y)
-    
-# plot EG grating vector
-plt.plot(kx_after_EG,ky_after_EG,'s-g',markersize='2')
-
-
+kx_after_EG, ky_after_EG = model_the_grating(kx_after_IG, ky_after_IG, m=1, lambda_grating=640, pitch=268.7, rotation_angle=45)
 ## OG (Output grating vector)
-# m = 1 # diffacted order
-# lambda_grating = 640 # in nm
-pitch = 380 # in nm (output grating)
-rotation_angle = np.deg2rad(-180)
-kx_unit_vector = 1
-ky_unit_vector = 1
-k3_x = (m*lambda_grating/pitch)*(np.cos(rotation_angle)*kx_unit_vector)
-k3_y = (m*lambda_grating/pitch)*(np.sin(rotation_angle)*ky_unit_vector)
+kx_after_OG, ky_after_OG = model_the_grating(kx_after_EG, ky_after_EG, m=1, lambda_grating=640, pitch=380, rotation_angle=-180)
 
-kx_after_OG = []
-ky_after_OG = []
-for i in range(0,len(kx)):
-    kx_after_OG.append(kx_after_EG[i]+k3_x)
-    ky_after_OG.append(ky_after_EG[i]+k3_y)
-    
-# plot EG grating vector
+## k-space to cartesian
+hfov_after_OG, vfov_after_OG = k_space_to_cartesian(kx_after_OG,ky_after_OG)
+  
+# plot grating vector
+plt.plot(kx_after_IG,ky_after_IG,'s-y',markersize='2')
+plt.plot(kx_after_EG,ky_after_EG,'s-g',markersize='2')
 plt.plot(kx_after_OG,ky_after_OG,'s-r',markersize='2')
-
 # plot inner TIR limit circle
-radius = 1 # in air
-angle = np.linspace(0, 2*np.pi, 100) 
-a = radius * np.cos(angle) 
-b = radius * np.sin(angle) 
-plt.plot(a,b,linewidth='1')
-
+plt_circle()
 # plot outer limit circle
-substrate_index = 2 # in substrate
-max_angle_allowed_inside_waveguide = np.deg2rad(80)
-radius = substrate_index * np.sin(max_angle_allowed_inside_waveguide)
-angle = np.linspace(0, 2*np.pi, 100) 
-a = radius * np.cos(angle) 
-b = radius * np.sin(angle) 
-plt.plot(a,b,linewidth='1')
+plt_circle(radius = substrate_index * np.sin(max_angle_allowed_inside_waveguide))
 
-
+# =============================================================================
+#  plot #2 setting
+# =============================================================================
 plt.title('k-space',fontsize='12')
 plt.xlabel('kx',fontsize='12')
 plt.ylabel('ky',fontsize='12')
@@ -173,44 +159,15 @@ plt.ylim([-2,2])
 plt.legend(['input','after IG','after EG','after OG','inner TIR limit','outer limit'],loc='upper right',fontsize=6)
 plt.gca().set_aspect('equal','box')
 
-#%%
-# k-space to cartesian
-kz = []
-theta_x_after_OG = []
-theta_y_after_OG = []
-for i in range(0,len(kx_after_OG)):
-    # Define kz 
-    kz.append(np.sqrt(1-kx_after_OG[i]**2-ky_after_OG[i]**2))
-    theta_x_after_OG.append(np.rad2deg(np.arctan2(kx[i],kz[i])))
-    theta_y_after_OG.append(np.rad2deg(np.arctan2(ky[i],kz[i])))
-
+# =============================================================================
+# plot #1 setting
+# =============================================================================
 plt.figure(dpi=600)
-plot_cartesian_space(hfov,vfov,legend = 'input',style='s-b')
-plot_cartesian_space(theta_x_after_OG,theta_y_after_OG,legend = 'output',style='s-r')
-# plt.plot(theta_x_after_OG,theta_y_after_OG,'s-r')
-#%%    
-    # Define θ (theta)
-    theta_x = np.deg2rad(hfov[i])
-    theta_y = np.deg2rad(vfov[i])
-    tan_theta_x = np.tan(theta_x)
-    tan_theta_y = np.tan(theta_y)
-    theta = np.rad2deg(np.arctan(np.sqrt((tan_theta_x**2)+(tan_theta_y**2))))
-    # Define φ (phi)
-    # if tan_theta_x == 0:
-    #     if tan_theta_y > 0:
-    #         phi = 90
-    #     else: 
-    #         phi = -90
-    # else:
-    #     phi = np.rad2deg(np.arctan2(tan_theta_y,tan_theta_x))
-    # Calculate kx & ky
-    kx.append(np.cos(np.deg2rad(phi)) * np.sin(np.deg2rad(theta)))
-    ky.append(np.sin(np.deg2rad(phi)) * np.sin(np.deg2rad(theta)))
-    
-# plot grating vector
-plt.plot(kx,ky,'s-b',markersize='2')
-#%%
+plt_cartesian_space(hfov,vfov,style='s-b')
+plt_cartesian_space(hfov_after_OG,vfov_after_OG,style='s-r')
+plt.legend(['input','output'])
 
+#%%
 
 """
 #%%
