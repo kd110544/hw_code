731da5acd292a968b419827d2b45c96adf0de5e0
JEFFCHAN-ACER-SWIFTSF514 <cwchan@arizona.edu>
2022-10-17 16:44:29 -0700

    add splitting lens program automation

---
+++ splitting_lens_with_min_SA.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+SPHERICAL ABERRATION: EFFECT OF SPLITTING LENS
+
+Program of designing a lens with minimum SA elements 
+
+@author: Jeff Ching-wen Chan
+@Created on Mon Oct 17 15:54:34 2022
+"""
+print()
+print("num: # of lens elements ")
+print("f:   focal length in mm")
+print("n:   refractive index (ex: NBK7 = 1.5168)")
+
+system_spec = input("\n Enter num, f, and n: \n (ex: 3 100 1.5168)\n      ").split()
+num = int(system_spec[0])
+f = float(system_spec[1])  # Focal length in mm
+n = float(system_spec[2])  # Glass material
+
+print(f"\n You enter the system spec: num={num}, f={f}, n={n} ")
+print("\n------------------------------")
+print(f"Design table for {num} element lens system with minimum SA: ")
+print("------------------------------")
+
+print("System Power\n")
+power_sys = 1/f   # System power 
+power = []          # power of each element 
+for i in range(0,num):
+    power.append(power_sys/num)
+    print(f"L{i+1}_power = {power[i]}")
+print("------------------------------")
+
+"""Step1: Calculate paraxial marginal ray slopes"""
+print("Paraxial Marginal Ray Slopes\n")
+h = 1 # Assume the margianl ray heigth in the object space is 1mm
+u = []
+u_tilt = []
+u.append(0) # Object is located at infinity
+for i in range(0,num):
+    u_tilt.append(u[i]-h*power[i])
+    u.append(u_tilt[i])
+    print(f"u{i+1}  = {u[i]}")
+    print(f"u{i+1}' = {u_tilt[i]}")
+u.remove(u[-1])
+print("------------------------------")
+
+"""Step2: Calculate conjugate factors"""
+print("Conjugate Factors\n")
+c = []
+for i in range(0,num):
+    c.append((u[i]+u_tilt[i])/(u[i]-u_tilt[i]))
+    print(f"C{i+1} = {c[i]}")
+print("------------------------------")
+
+"""Step3: Calculate the optimum shape factors"""
+print("Optimum Shape Factors\n")
+b_opt = []
+for i in range(0,num):
+    b_opt.append(-2*(n**2-1)*c[i]/(n+2))
+    print(f"B{i+1}_opt = {b_opt[i]}")
+print("------------------------------")
+
+"""Step4: Calculate radius of surfaces"""
+print("Radius of Surfaces\n")
+r = []
+for i in range(0,num):
+    r.append(2*(n-1)/(power[i]*(b_opt[i]+1)))
+    print(f"R{2*i+1} = {r[2*i]}")
+    r.append(2*(n-1)/(power[i]*(b_opt[i]-1)))
+    print(f"R{2*i+2} = {r[2*i+1]}")
+    
+
---
+++ three_element_lens_design_OPTI340A_.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+THREE ELEMENT LENS DESIGN
+
+Design a three element lens system for which spherical aberration is 
+individually minimized at each of the element.
+
+System requirement:
+    F/number:5
+    f = 100 mm
+    Wavelength:587 nm (d-line)
+    FOV: 0, 1, and 2 degs
+    Object: at infinity
+    Glass material: NBK7
+    # of lens elements = 3
+
+@author: Jeff Ching-wen Chan
+@Created on Mon Oct 17 13:06:30 2022
+"""
+
+n = 1.5168  # Glass material = NBK7
+f = 100    # Focal length in mm
+number_of_element = 3
+
+power_sys = 1/f   # System power 
+phi = []          # power of each element       
+for i in range(0,number_of_element):
+    phi.append(power_sys/number_of_element)
+
+"""Step1: Calculate paraxial marginal ray slopes"""
+h = 1 # Assume the margianl ray heigth in the object space is 1mm
+u = []
+u_tilt = []
+u.append(0) # Object is located at infinity
+for i in range(0,number_of_element):
+    u_tilt.append(u[i]-h*phi[i])
+    u.append(u_tilt[i])
+u.remove(u[-1])
+
+"""Step2: Calculate conjugate factors"""
+c = []
+for i in range(0,number_of_element):
+    c.append((u[i]+u_tilt[i])/(u[i]-u_tilt[i]))
+
+"""Step3: Calculate the optimum shape factors"""
+b_opt = []
+for i in range(0,number_of_element):
+    b_opt.append(-2*(n**2-1)*c[i]/(n+2))
+
+"""Step4: Calculate radius of surfaces"""
+r = []
+for i in range(0,number_of_element):
+    r.append(2*(n-1)/(phi[i]*(b_opt[i]+1)))
+    r.append(2*(n-1)/(phi[i]*(b_opt[i]-1)))
+
+
+
+#%% Step by step
+n = 1.5168  # Glass material = NBK7
+f = 100     # Focal length in mm
+power_sys = 1/f   # System power 
+power_L1 = power_sys/3
+power_L2 = power_sys/3
+power_L3 = power_sys/3
+
+# =============================================================================
+# Step1: Calculate paraxial marginal ray slopes, u1, u1',u2, u2', u3, and u3'. 
+# Assume the margianl ray heigth in the object space is 1mm. 
+# =============================================================================
+h1 = 1
+u1 = 0
+u1_tilt = u1-h1*power_L1
+u2 = u1_tilt
+u2_tilt = u2-h1*power_L2
+u3 = u2_tilt
+u3_tilt = u3-h1*power_L3
+
+# =============================================================================
+# Step2: Calculate conjugate factors C1, C2, and C3 for Lens #1, #2, and #3.
+# =============================================================================
+c1 = (u1+u1_tilt)/(u1-u1_tilt)
+c2 = (u2+u2_tilt)/(u2-u2_tilt)
+c3 = (u3+u3_tilt)/(u3-u3_tilt)
+
+# =============================================================================
+# Step3: Calculate the optimum shape factor b1_opt, b2_opt, and b3_opt
+# =============================================================================
+b1_opt = -2*(n**2-1)*c1/(n+2)
+b2_opt = -2*(n**2-1)*c2/(n+2)
+b3_opt = -2*(n**2-1)*c3/(n+2)
+
+# =============================================================================
+# Step4: Based on optimum value and refractive index, calculate r1, r2, r3, and r4 by using the procedure for singlet.
+# =============================================================================
+r1 = 2*(n-1)/(power_L1*(b1_opt+1))
+r2 = 2*(n-1)/(power_L1*(b1_opt-1))
+r3 = 2*(n-1)/(power_L2*(b2_opt+1))
+r4 = 2*(n-1)/(power_L2*(b2_opt-1))
+r5 = 2*(n-1)/(power_L3*(b3_opt+1))
+r6 = 2*(n-1)/(power_L3*(b3_opt-1))
